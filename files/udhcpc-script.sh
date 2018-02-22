d225ed73635facf0599312cda35dddd1c8ac4a3f
Sean Ho <git@seanho.com>
2018-02-22 00:11:03 -0800

    Android: move DNS hacking to udhcpc script

--- files/udhcpc-script.sh
+++ files/udhcpc-script.sh
@@ -4,16 +4,22 @@
 ## https://udhcp.busybox.net/README.udhcpc
 ## key vars: interface ip subnet mask broadcast router dns domain
 
+pri=4000
+tid=200
+
 [ -n "$1" ] || {
   echo "Error: should be called from udhcpc"
   exit 1
 }
 
 case "$1" in
-	deconfig)
-		echo "Deconfiguring interface: $interface"
-		ip link set $interface up
-		;;
+ deconfig)
+  echo "Deconfiguring interface: $interface"
+  ip link set $interface up
+  ip ru del pri $pri lookup $tidy
+  ip ro flush table $tid
+  iptables -t nat -D OUTPUT -j out_dns 2>&-
+  ;;
 
  bound)
   echo "Setting IP address $ip on $interface"
@@ -34,6 +40,22 @@ case "$1" in
 				route add default gw $i dev $interface metric $((metric++))
 			done
 		fi
+
+redirectDNS() {
+  echo "Redirecting DNS traffic to $1"
+
+  chain="out_dns"
+  nat="iptables -t nat"
+  rule="--dport 53 -j DNAT --to $1"
+
+  $nat -N $chain 2>&-
+  $nat -F $chain
+  $nat -A $chain -p udp $rule
+  $nat -A $chain -p tcp $rule
+  $nat -C OUTPUT -j $chain 2>&- || \
+  $nat -I OUTPUT -j $chain
+}
+
 		;;
 esac
 
